bc3886ce2924ccfb7c8871e1bea74c65850020ba
Jordan Jones <jones-j8@ulster.ac.uk>
2017-10-08 19:23:19 +0100

    Add gitignore file
    Add basic db interface

--- app.js
+++ app.js
@@ -3,10 +3,14 @@
 const express = require('express');
 const app = express();
 
+const db = require('./lib/database/database.js');
+
 const port = process.env.PORT || 3000;
 
 require('./routes/index')(app);
 
 app.listen(port, () => {
    console.log('Server listening on port %s', port);
+
+   db.get('users', {});
 });
