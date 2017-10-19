36b9af604173634aa6c52ce29fa93edd83bda916
Jordan Jones <jones-j8@ulster.ac.uk>
2017-10-19 19:42:45 +0100

    Add grunt task to populate dbConnection config file

--- GruntFile.js
+++ GruntFile.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 module.exports = (grunt) => {
 
     grunt.initConfig({
@@ -13,4 +15,15 @@ module.exports = (grunt) => {
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
     grunt.registerTask('lint', ['jshint']);
+
+    grunt.registerTask('createDbConfig', () => {
+        let dbConf = {
+            user: grunt.option('user'),
+            pass: grunt.option('pass'),
+            hosts: grunt.option('host'),
+            opts: grunt.option('opts')
+        };
+        fs.writeFileSync('./config/dbConnection.json', JSON.stringify(dbConf),
+            {encoding: 'utf8', flag: 'wx'});
+    });
 };
