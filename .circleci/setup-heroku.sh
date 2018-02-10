29c07fd5d0669aa2a11ce4b727dbf848eb38d4ad
jjones09 <jjones09@live.co.uk>
2018-02-10 17:54:06 +0000

    add sudo to tar command etc

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -4,10 +4,10 @@ set -eu
 
 git remote add heroku https://git.heroku.com/$HEROKU_APP_NAME.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
-tar -xvsf heroku-linux-amd64.tar.gz -C /usr/local/lib
-ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+sudo tar -xvsf heroku-linux-amd64.tar.gz -C /usr/local/lib
+sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
-cat > ~/.netrc << EOF
+sudo cat > ~/.netrc << EOF
 machine api.heroku.com
         login $HEROKU_LOGIN
         password $HEROKU_API_KEY
@@ -16,5 +16,5 @@ machine git.heroku.com
         password $HEROKU_API_KEY
 EOF
 
-chmod 600 ~/.netrc
-ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
+sudo chmod 600 ~/.netrc
+sudo ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
