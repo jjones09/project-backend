99f2dc857b4eb7de07cf4b86dec49365993958c6
jjones09 <jjones09@live.co.uk>
2018-03-23 17:37:43 +0000

    Add route to access log file

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,11 @@
 "use strict";
 
-const https = require('https');
-
-// const userBase = require('../lib/firebase-interface/firebaseInterface');
-const tokenMgr = require('../lib/token-manager/tokenManager');
-
-const userDB = require('../lib/user-db-manager/userDbManager');
-
+const fs = require("fs");
 
 module.exports = router => {
 
     router.route('/logs')
         .get((req, res) => {
-
+            res.send(fs.readFileSync('./app.log'));
         });
 };
