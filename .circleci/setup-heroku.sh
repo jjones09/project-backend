6b757c9ebadf40a07c3c478e81665f0cedc8fe35
jjones09 <jjones09@live.co.uk>
2018-03-24 04:09:38 +0000

    update setup-heroku.sh and add ssh keys within config yml

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,20 +1,16 @@
-#!/bin/bash
+ #!/bin/bash
+ wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+ sudo mkdir -p /usr/local/lib /usr/local/bin
+ sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+ sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
-set -eu
+ cat > ~/.netrc << EOF
+ machine api.heroku.com
+   login $HEROKU_LOGIN
+   password $HEROKU_API_KEY
+ EOF
 
-git remote add heroku https://git.heroku.com/$HEROKU_APP_NAME.git
-wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
-sudo tar -xvsf heroku-linux-amd64.tar.gz -C /usr/local/lib
-sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
-
-sudo cat > ~/.netrc << EOF
-machine api.heroku.com
-        login $HEROKU_LOGIN
-        password $HEROKU_API_KEY
-machine git.heroku.com
-        login $HEROKU_LOGIN
-        password $HEROKU_API_KEY
-EOF
-
-sudo chmod 600 ~/.netrc
-sudo ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
+ cat >> ~/.ssh/config << EOF
+ VerifyHostKeyDNS yes
+ StrictHostKeyChecking no
+ EOF
