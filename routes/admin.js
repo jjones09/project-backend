7dfd66be934a4df1defbd4899feb140e18081f18
jjones09 <jjones09@live.co.uk>
2018-03-24 02:50:17 +0000

    add middleware to auth routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,22 @@
 "use strict";
 
-const fs = require("fs");
+const fs = require('fs');
+
+const auth = require('http-auth');
+
+// TODO Integrate actual credentials for admin access
+let basic = auth.basic(
+    { realm: "Admin"},
+    (user, pass, cb) => {
+        cb(user === "foo" && pass === "bar");
+    });
+
+let middleware = auth.connect(basic);
 
 module.exports = router => {
 
     router.route('/logs')
-        .get((req, res) => {
+        .get(middleware, (req, res) => {
             res.send(fs.readFileSync('./app.log'));
         });
 };
